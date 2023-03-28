e1c5f8cbb8317699ed0226799afc64e8ca4e4589
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-03-28 17:10:22 -0400

    Update 2size.py

--- pages/2size.py
+++ pages/2size.py
@@ -1,11 +1,24 @@
+import streamlit as st
 from PIL import Image
 
-# Load the image
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg","jpeg","png"])
+# Set up Streamlit app
+st.set_page_config(page_title="Image Resizer", layout="wide")
+st.title("Image Resizer")
 
-# Resize the image
-new_size = (500, 500)
-uploaded_file = uploaded_file.resize(new_size)
+# Upload image
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-# Display the image
-st.image(uploaded_file, caption='My Image')
+# If image is uploaded
+if uploaded_file is not None:
+    # Open uploaded image with Pillow
+    img = Image.open(uploaded_file)
+
+    # Display original image
+    st.subheader("Original Image")
+    st.image(img, use_column_width=True)
+
+    # Display image in different sizes
+    st.subheader("Resized Images")
+    for size in [100, 200, 500]:
+        resized_img = img.resize((size, size))
+        st.image(resized_img, caption=f"{size} x {size}", use_column_width=True)
