2f35ae54d878bdf064b20e5a221b86b8965c3d72
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-03-28 15:07:40 -0400

    Update smartfashion.py

--- smartfashion.py
+++ smartfashion.py
@@ -9,16 +9,20 @@ from feature_extractor import FeatureExtractor
 
 # Read image features
 fe = FeatureExtractor()
+st.write('prnt1')
 features = []
+st.write('prnt2')
 img_paths = []
+st.write('prnt3')
 for feature_path in Path("./static/feature").glob("*.npy"):
+    st.write('prnt4')
     features.append(np.load(feature_path))
-    st.write('prnt')
+    st.write('prnt5')
     img_paths.append(Path("./static/img") / (feature_path.stem + ".jpg"))
-    st.write('prnt1')
+    st.write('prnt6')
 features = np.array(features)
 
-
+st.write('prnt7')
 st.set_page_config(
     page_title="Hello",
     page_icon="👋",
