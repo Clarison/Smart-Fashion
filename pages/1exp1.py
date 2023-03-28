1a2b345e0aafda82775f09f1c406b866a92d04c8
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-03-28 15:50:43 -0400

    Update 1exp1.py

--- pages/1exp1.py
+++ pages/1exp1.py
@@ -6,37 +6,48 @@ from flask import Flask, request, render_template
 from pathlib import Path
 from feature_extractor import FeatureExtractor
 
-if __name__ == '__main__':
-    fe = FeatureExtractor()
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)       
+  
+    
+st.write("# Welcome to Streamlit by Group 6! 👋")
 
-    for img_path in sorted(Path("./static/img").glob("*.jpg")):
+# Read image features
+fe = FeatureExtractor()
+
+for img_path in sorted(Path("./static/img").glob("*.jpg")):
         print(img_path)  # e.g., ./static/img/xxx.jpg
         feature = fe.extract(img=Image.open(img_path))
         feature_path = Path("./static/feature") / (img_path.stem + ".npy")  # e.g., ./static/feature/xxx.npy
         np.save(feature_path, feature)
         
-        
-# Read image features
-fe = FeatureExtractor()
-st.write('prnt1')
+
 features = []
-st.write('prnt2')
 img_paths = []
-st.write('prnt3')
 for feature_path in Path("./static/feature").glob("*.npy"):
-    st.write('prnt4')
     features.append(np.load(feature_path))
-    st.write('prnt5')
     img_paths.append(Path("./static/img") / (feature_path.stem + ".jpg"))
-    st.write('prnt6')
 features = np.array(features)
 
-st.write('prnt7')
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
 
-st.write("# Welcome to Streamlit by Group 6! 👋")
+# Create a file uploader with a label and type of image
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
+# Check if an image was uploaded
+if uploaded_file is not None:
+    # Display the image
+    st.image(uploaded_file)
+
+    
+# Run search
+query = fe.extract(uploaded_file)
+dists = np.linalg.norm(features-query, axis=1)  # L2 distances to features
+ids = np.argsort(dists)[:30]  # Top 30 results
+scores = [(dists[id], img_paths[id]) for id in ids]
+
+
+st.write(scores)
+        
 st.write(features)
