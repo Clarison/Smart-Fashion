6bb12b6b9acd025be819240d2dba6f9d6c4f04df
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-03-31 13:38:52 -0400

    Update 4dlle.py

--- pages/4dlle.py
+++ pages/4dlle.py
@@ -2,28 +2,36 @@ import streamlit as st
 import asyncio
 from DALLE import DALLE
 
+st.write('Hi there!')
 
-st.write('hi')
+# Instantiate DALLE model
 dalle = DALLE("sess-xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
+# Define async function to generate images
 async def generate_images(prompt):
     images = await dalle.generate(prompt)
     return images
 
-
-st.title("DALLE Image Generation App")
+# Define Streamlit app
+def app():
+    st.title("DALLE Image Generation App")
 
     # Get text prompt from user
-prompt = st.text_input("Enter a text prompt", "Kitten")
+    prompt = st.text_input("Enter a text prompt", "Kitten")
 
     # Generate images when the user clicks the button
-if st.button("Generate Images"):
-        # Generate the images asynchronously
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    images = loop.run_until_complete(generate_images(prompt))
-    st.write("Generating images...")
+    if st.button("Generate Images"):
+        # Display loading message
+        with st.spinner("Generating images..."):
+            # Generate the images asynchronously
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            images = loop.run_until_complete(generate_images(prompt))
 
-        # Display the images
-for image in images:
-    st.image(image)
+            # Display the images
+            for image in images:
+                st.image(image, use_column_width=True)
+
+# Run the app
+if __name__ == "__main__":
+    app()
