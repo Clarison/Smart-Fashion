28872679e62e51ba380fd37431e07ebd7eb2cde1
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-03-31 13:27:52 -0400

    Update 4dlle.py

--- pages/4dlle.py
+++ pages/4dlle.py
@@ -1,5 +1,28 @@
-import DALLE, asyncio
 import streamlit as st
-dalle = DALLE.DALLE("sess-xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-st.write("Generating images...")
-images = await dalle.generate("Kitten")
+import asyncio
+from DALLE import DALLE
+
+dalle = DALLE("sess-xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+async def generate_images(prompt):
+    images = await dalle.generate(prompt)
+    return images
+
+def app():
+    st.title("DALLE Image Generation App")
+
+    # Get text prompt from user
+    prompt = st.text_input("Enter a text prompt", "Kitten")
+
+    # Generate images when the user clicks the button
+    if st.button("Generate Images"):
+        # Generate the images asynchronously
+        task = asyncio.create_task(generate_images(prompt))
+        st.write("Generating images...")
+
+        # Wait for the images to be generated
+        images = await task
+
+        # Display the images
+        for image in images:
+            st.image(image)
